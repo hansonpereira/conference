50f7feda15be19bdf983053e0ea3766a5e5e2495
Hanson Pereira <hanson.robert.pereira@gmail.com>
2025-03-30 17:53:16 +0300

    Addons phase-1 implemented

--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
+import avReducer from './avSlice';
 import venueReducer from './venueSlice';
-
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: avReducer
   },
 });
